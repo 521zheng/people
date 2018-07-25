1d705fc3061c53861fe68adbd9214a6f40612760
leesia <leesia@aliyun.com>
2018-07-26 03:03:52 +0800

    修改工程代码

--- resource/src/main/java/org/leesia/resource/controller/TestController.java
+++ resource/src/main/java/org/leesia/resource/controller/TestController.java
@@ -1,11 +1,9 @@
 package org.leesia.resource.controller;
 
-import org.leesia.resource.redis.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Auther: leesia
@@ -16,33 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "test")
 public class TestController {
 
-    @Autowired
-    RedisService redisService;
 
-    @RequestMapping(value = "reversal", method = RequestMethod.GET)
+    @RequestMapping(value = "test", method = RequestMethod.GET)
     @ResponseBody
     public Object reversal(HttpServletRequest request, @RequestParam String param) {
-        if (param == null) {
-            return "";
-        }
-
-        String v = redisService.getString(param);
-        if (v == null) {
-            String rev =  new StringBuilder(param).reverse().toString();
-            redisService.setString(param, rev);
-            return rev;
-        }
-        return v;
-    }
-
-    @RequestMapping(value = "delete", method = RequestMethod.GET)
-    @ResponseBody
-    public Object delete(HttpServletRequest request, @RequestParam String param) {
-        String v = redisService.getString(param);
-        if (v != null) {
-            redisService.deleteString(param);
-            return true;
-        }
-        return false;
+        return param;
     }
 }
